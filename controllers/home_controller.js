e0499a7373460abc92302d7fec2b695cc39dd3f6
Vishal Kwatra <Vishal.Kwatra@gds.ey.com>
2020-09-18 23:22:03 +0530

    Initial database working

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,15 +1,27 @@
 const Task = require('../models/tasksList');
-var tasks_list = [
-    {
-        desc: "Annual report submission deadline",
-        category: "Work",
-        date: new Date(2020, 09, 14)
-    }
-];
-module.exports.tasks_list = tasks_list;
+// var tasks_list = [
+//     {
+//         desc: "Annual report submission deadline",
+//         category: "Work",
+//         date: new Date(2020, 09, 14)
+//     }
+// ];
+// module.exports.tasks_list = tasks_list;
 module.exports.home = function (req, res) {
-    res.render('home',{
-        title: "To Do List",
-        tasks_list: tasks_list
-    });
-  }
+    Task.find({}, function (err, tasks) {
+        if (err) {
+            console.log('Error in fetching tasks from db');
+            return;
+        }
+
+        res.render('home', {
+            title: "To Do List",
+            tasks_list: tasks
+        });
+
+    })
+    // res.render('home',{
+    //     title: "To Do List",
+    //     tasks_list: tasks_list
+    // });
+}
--- controllers/tasks_controller.js
+++ controllers/tasks_controller.js
@@ -1,24 +1,59 @@
 const homeController = require('../controllers/home_controller');
+const Task = require('../models/tasksList');
+
 module.exports.addTask = function (req, res) {
     console.log('In Add Task');
-    homeController.tasks_list.push({
+    // homeController.tasks_list.push({
+    //     desc: req.body.desc,
+    //     category: req.body.category,
+    //     date: req.body.date
+    // });
+    // return res.redirect('/');
+    Task.create({
         desc: req.body.desc,
         category: req.body.category,
         date: req.body.date
+    }, function (err, newTask) {
+        if (err) {
+            console.log('Error in adding new Task');
+            return;
+        }
+        console.log('*******', newTask);
+        return res.redirect('back');
     });
-    return res.redirect('/');
+
 }
 
 module.exports.deleteTask = function (req, res) {
+    // console.log('In Delete Task');
+    // console.log(req.body);
+    // for (let index = 0; index < req.body.descr.length; index++) {
+    //     let desc = req.body.descr[index];
+    //     if (req.body.descr[index]) {
+    //         let ind = homeController.tasks_list.findIndex(task_item => task_item.desc === req.body.descr[index]);
+    //         let delElements = homeController.tasks_list.splice(ind, 1);
+    //     }
+
+    // }
+
+    // return res.redirect('back');
+
     console.log('In Delete Task');
     console.log(req.body);
     for (let index = 0; index < req.body.descr.length; index++) {
         let desc = req.body.descr[index];
         if (req.body.descr[index]) {
-            let ind = homeController.tasks_list.findIndex(task_item => task_item.desc === req.body.descr[index]);
-            let delElements = homeController.tasks_list.splice(ind, 1);
+            Task.deleteOne({ desc: req.body.descr[index] }, function (err) {
+                if (err) {
+                    console.log('Error in deleting the task from database');
+                    return;
+                }
+
+                return res.redirect('back');
+            })
         }
 
     }
-    return res.redirect('back');
+
+
 }
