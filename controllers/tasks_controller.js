915780f00e7d86d863e506c65ca95d3718b2da97
Vishal Kwatra <Vishal.Kwatra@gds.ey.com>
2020-09-18 16:18:20 +0530

    Memory storing to do list

--- controllers/tasks_controller.js
+++ controllers/tasks_controller.js
@@ -12,5 +12,13 @@ module.exports.addTask = function (req, res) {
 module.exports.deleteTask = function (req, res) {
     console.log('In Delete Task');
     console.log(req.body);
+    for (let index = 0; index < req.body.descr.length; index++) {
+        let desc = req.body.descr[index];
+        if (req.body.descr[index]) {
+            let ind = homeController.tasks_list.findIndex(task_item => task_item.desc === req.body.descr[index]);
+            let delElements = homeController.tasks_list.splice(ind, 1);
+        }
+
+    }
     return res.redirect('back');
 }
